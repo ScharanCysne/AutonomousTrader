52d99afaf0585d07b0f9a45cb5081b51ab814035
igoramdias <igor.am.dias@gmail.com>
2020-12-06 18:21:03 -0300

    Alteração para múltiplas ações

--- source/trader/Tiny RL/TinyRLAgent.py
+++ source/trader/Tiny RL/TinyRLAgent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import math
 
 from Analysis import Analysis
 
@@ -14,11 +15,12 @@ from Analysis import Analysis
     TODO: Optimize parameters M, T
 '''
 class Agent:
-    def __init__(self, epochs=1000, learning_rate=0.3, commission=0, capital=1000):
+    def __init__(self, epochs=1000, learning_rate=0.3, commission=0, capital=1000, prices):
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.commission = commission
         self.capital = capital
+        self.prices = prices
 
     '''
         Sharpe Ratio
@@ -29,65 +31,73 @@ class Agent:
     '''
         Ft(x^t.theta) position on the asset
     '''
-    def positions(self, x, theta):
-        M = len(theta) - 2
-        T = len(x)
-        Ft = np.zeros(T)
-        for t in range(M, T):
-            xt = np.concatenate([[1], x[t - M:t], [Ft[t - 1]]])
-            Ft[t] = np.tanh(np.dot(theta, xt))
-        return Ft
+    def positions(self, xs, thetas):
+        M = [len(theta) - 2 for theta in thetas]
+        T = [len(x) for x in xs]
+        Fts = [np.zeros(T[i]) for x in xs]
+        for i in range(len(xs)):
+            for t in range(M[i], T[i]):
+                xt = np.concatenate([[1], xs[i][t - M:t], [Fts[i][t - 1]]])
+                Fts[i][t] = np.tanh(np.dot(theta[i], xt))
+        return Fts
 
     '''
         Returns on current asset allocation
     '''
-    def returns(self, Ft, x, delta):
-        T = len(x)
-        rets = Ft[0:T - 1] * x[1:T] - delta * np.abs(Ft[1:T] - Ft[0:T - 1])
+    def returns(self, Fts, xs, delta, time):
+        rets_parc = [Fts[i][0:len(xs[i]) - 1] * xs[i][1:len(xs[i])] - delta * np.abs(Fts[i][1:len(xs[i])] - Fts[i][0:len(xs[i]) - 1]) for i in range(len(xs))]
+        dist = [np.mean(rets_parc[i])/np.sum(np.mean(rets_parc)) for i in range(len(xs))]
+        mi = [math.floor(self.capital*dist[i]/price[i][time]) for i in range(len(xs))] 
+        rets = np.dot(rets_parc,mi)
         return np.concatenate([[0], rets])
 
     '''
         Gradient Ascent to maximize Sharpe Ratio
     '''
-    def gradient(self, x, theta, delta):
-        Ft = self.positions(x, theta)
-        R = self.returns(Ft, x, delta)
-        T = len(x)
-        M = len(theta) - 2
+    def gradient(self, xs, theta, delta, time):
+        Ft = self.positions(xs, theta)
+        R = self.returns(Ft, xs, delta, time)
+        Ss = np.zeros(len(xs))
+        grads = np.zeros(len(xs))
+        for i in range(len(xs)):
+            T = len(xs[i])
+            M = len(thetas[i])
         
-        A = np.mean(R)
-        B = np.mean(np.square(R))
-        S = A / np.sqrt(B - A ** 2)
+            A = np.mean(R[i])
+            B = np.mean(np.square(R[i]))
+            S = A / np.sqrt(B - A ** 2)
 
-        dSdA = S * (1 + S ** 2) / A
-        dSdB = -S ** 3 / 2 / A ** 2
-        dAdR = 1. / T
-        dBdR = 2. / T * R
+            dSdA = S * (1 + S ** 2) / A
+            dSdB = -S ** 3 / 2 / A ** 2
+            dAdR = 1. / T
+            dBdR = 2. / T * R
         
-        grad = np.zeros(M + 2)  # initialize gradient
-        dFpdtheta = np.zeros(M + 2)  # for storing previous dFdtheta
+            grad = np.zeros(M + 2)  # initialize gradient
+            dFpdtheta = np.zeros(M + 2)  # for storing previous dFdtheta
         
-        for t in range(M, T):
-            xt = np.concatenate([[1], x[t - M:t], [Ft[t-1]]])
-            dRdF = -delta * np.sign(Ft[t] - Ft[t-1])
-            dRdFp = x[t] + delta * np.sign(Ft[t] - Ft[t-1])
-            dFdtheta = (1 - Ft[t] ** 2) * (xt + theta[-1] * dFpdtheta)
-            dSdtheta = (dSdA * dAdR + dSdB * dBdR[t]) * (dRdF * dFdtheta + dRdFp * dFpdtheta)
-            grad = grad + dSdtheta
-            dFpdtheta = dFdtheta
+            for t in range(M, T):
+                xt = np.concatenate([[1], xs[i][t - M:t], [Fts[i][t-1]]])
+                dRdF = -delta * np.sign(Fts[i][t] - Fts[i][t-1])
+                dRdFp = xs[i][t] + delta * np.sign(Fts[i][t] - Fts[i][t-1])
+                dFdtheta = (1 - Fts[i][t] ** 2) * (xt + theta[i][-1] * dFpdtheta)
+                dSdtheta = (dSdA * dAdR + dSdB * dBdR[t]) * (dRdF * dFdtheta + dRdFp * dFpdtheta)
+                grad = grad + dSdtheta
+                dFpdtheta = dFdtheta
 
-        return grad, S
+            Ss[i] = S
+            grads[i] = grad
+        return grads, Ss
 
     '''
         Training Reinforment 
     '''
-    def train(self, x, epochs=2000, M=8, commission=0.0025, learning_rate = 0.3):
-        theta = np.random.rand(M + 2)
+    def train(self, xs, epochs=2000, M=8, commission=0.0025, learning_rate = 0.3):
+        thetas = [np.random.rand(M + 2) for x in xs]
         sharpes = np.zeros(epochs) # store sharpes over time
         for i in range(epochs):
             print(f"Training Epoch {i}")
-            grad, sharpe = self.gradient(x, theta, commission)
-            theta = theta + grad * learning_rate
+            grads, sharpes = self.gradient(xs, thetas, commission, i)
+            thetas = thetas + grads * learning_rate
             sharpes[i] = sharpe
         
         print("Finished Training")
--- source/trader/Tiny RL/agent.py
+++ source/trader/Tiny RL/agent.py
@@ -2,24 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
 
 from Analysis import Analysis
 from TinyRLAgent import Agent
 
-#stocks = ["BBAS3", "BBDC4", "ITUB4", "PETR4", "VALE3"]
-#portfolio = np.array([pd.read_csv(f"input/{stock}.csv", sep="\t") for stock in stocks], dtype=object)
-#rets = [stock['<CLOSE>'].diff()[1:] for stock in portfolio]
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 # Load stock to trains TinyRL
-stock = pd.read_csv("input/ITUB4.csv", sep="\t")
+stocks = ["BBAS3", "BBDC4", "ITUB4", "PETR4", "VALE3"]
+portfolio = np.array([pd.read_csv(os.path.join(THIS_FOLDER, f'input\\{stock}.csv'), sep="\t") for stock in stocks], dtype=object)
+rets = [stock['<CLOSE>'].diff()[1:] for stock in portfolio]
+prices = [stock['<CLOSE>'] for stock in portfolio]
+for i in range(len(stocks)):
+    rets[i].name = stocks[i]
+    prices[i].name = stocks[i]
+
+'''
+csv_filename = os.path.join(THIS_FOLDER, 'input\\ITUB4.csv')
+stock = pd.read_csv(csv_filename, sep="\t")
 stock = stock.dropna()
+
 rets = np.array(stock['<CLOSE>'].diff()[1:])
+''' 
 
 # Separating Train and Test Samples
 N = int(0.7 * len(rets))            # Number of Training Samples
 P = len(rets) - N                  # Number of Test Samples
-rets_train = rets[:N]             # Training Samples
-rets_test = rets[-P:]             # Test Samples
+rets_train = [rets[:N] for ret in rets]        # Training Samples
+rets_test = [rets[-P:] for ret in rets]        # Test Samples
 
 # Hyper-parameters
 epochs = 2000
@@ -28,13 +39,13 @@ commission = 0.0025
 capital = 10000
 
 # Reinforcement Learning Agent
-analyst = Analysis(stock)
-tinyrl = Agent(capital, analyst)
+analyst = Analysis(prices)
+tinyrl = Agent(capital = capital, prices = prices)
 # Train Tiny RL in training samples
-theta, sharpes = tinyrl.train(rets_train, epochs=2000, M=8, commission=0.0025, learning_rate=0.3)
+theta, sharpes = tinyrl.train(rets_train, epochs=2500, M=5, commission=0.0025, learning_rate=0.001)
 
 # Check results from training and testing
-train_returns = tinyrl.returns(tinyrl.positions(rets_train, theta), rets_test, 0.0025)
+train_returns = tinyrl.returns(tinyrl.positions(rets_train, theta), rets_train, 0.0025)
 test_returns = tinyrl.returns(tinyrl.positions(rets_test, theta), rets_test, 0.0025)
 
 
@@ -63,4 +74,4 @@ plt.xlabel('Ticks')
 plt.ylabel('Cumulative Returns')
 plt.legend()
 plt.title("RL Model vs. Buy and Hold - Test Data")
-plt.show()
+plt.show()'''
